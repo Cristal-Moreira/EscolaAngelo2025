68154daaa24f39b866cdbf9048778f8a78245062
Cristal-Moreira <cristal.moreira08@gmail.com>
2025-03-14 10:16:22 -0300

    atualização

--- aula20.py
+++ aula20.py
@@ -3,8 +3,22 @@
 # 1. Na lista pares = [0, 2, 4, 8]:
 # a) Acrecente o valor 10 ao final da lista.
 # b) Acrecente o valor 6 na posição 3.
+pares = [0, 2, 4, 8]
+pares.append(10)
+pares.insert(3, 6)
+print(pares)# 2. Na lista impares = [1, 3, 3, 5, 7, 9]:
 
-# 2. Na lista impares = [1, 3, 3, 5, 7, 9]:
 # a) Remova o valor 3.
 # b) Remova o valor da posição (indice) 4.
 # c) Mostre o valor que sera removido da posição (indice)
+impares = [1, 3, 3, 5, 7, 9]
+impares.remove(3)
+valor_removido = impares.pop(4)
+print(valor_removido)
+print(impares)
+
+# 3. Na lista fibonacci = [8, 1, 0, 5, 13, 1, 3, 2]:
+# a) ordene a lista.
+# b) Coloque em valor reverso a lista fibonacci.]
+
+# 4. Na lista 
