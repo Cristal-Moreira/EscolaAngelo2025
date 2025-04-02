dd5c483c4206bad8fecfc3de292ffc8966e39fc3
Cristal-Moreira <cristal.moreira08@gmail.com>
2025-04-02 09:20:43 -0300

    .

---
+++ aula29.py
@@ -0,0 +1,17 @@
+# TUPLA X LISTA
+
+lista_vazia = []
+print(lista_vazia)
+lista_vazia.append("Bom dia")
+lista_vazia.append("Olá, bom dia")
+print("lista_vazia")
+
+tupla = ()
+print(tupla)
+tupla = lista(("Ana", "Paula", "Sophia"))
+tupla.append("Cristal")
+tupla = tuple(tupla)
+prit(tupla)
+
+print(lista_vazia[0])
+print(tupla[3])
